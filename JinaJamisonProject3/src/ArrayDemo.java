aaaf8140571de8121153b2c27001428a38def1ab
jamie jamison <jamison@library.ucla.edu>
2018-04-25 14:23:01 -0700

    20180425jmj: Class Project3 completed

--- JinaJamisonProject3/src/ArrayDemo.java
+++ JinaJamisonProject3/src/ArrayDemo.java
@@ -2,13 +2,13 @@
 import java.util.Scanner;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *  Project3 
+ *  Winter quarter 2018
+ *  Java class at ucla extension
  */
 
 /**
- *  1) Use Scanner method from the video
+ *  1) Use Scanner method from the video sample for input
  * @author jamie
  */
         
@@ -20,25 +20,26 @@ public class ArrayDemo {
         
         for (int i=0; i < data.length; i++)
         {
-            System.out.println("Enter an value for index1 " + i + ":");
+            System.out.println("Enter a value (" + (i+1) + " of 5): ");
             data[i] = in.nextInt();
         }
         
-        display(data);
-        
+        display(data);        
+           
         float numberAverage = getAverage(data);
-        System.out.println("Average of numbers: " + numberAverage);
+        System.out.println("Average of the numbers is: " + numberAverage);
         
+        int largestNumber = getLargest(data);
+        System.out.println("The largest number is: " + largestNumber);
         
+        int smallestNumber = getSmallest(data);
+        System.out.println("The smallest number is: " + smallestNumber);
     }
     
-    /*      
-     *   More from the scanner demo, display the integer values in the array
-     *
-    */
+    /* From the scanner demo, display the integer values in the array */
     static void display(int[] values)
     {
-        System.out.println("You entered the following numbers: ");
+        System.out.println("You've entered the following numbers: ");
         for (int i=0; i<values.length; i++)
         {
             System.out.print(values[i] + " ");
@@ -48,38 +49,54 @@ public class ArrayDemo {
     
     static float getAverage(int[] data)
     {
-        float total=0;
-        float average=0;
+        float total=0,average=0;
         
         for (int i=0; i<data.length; i++)
         {
            total = total + data[i];
-           System.out.println("Total = " + total);
+        /*
+        * debugging statements
+        *  System.out.println("Total = " + total);
+         */
         }
         
         /*
         * debugging statements
+        * System.out.println("Length = " + data.length);
+        * System.out.println("Average = " + average);
+        * System.out.println("Total = " + total);
          */
-        System.out.println("Length = " + data.length);
-        System.out.println("Average = " + average);
-        System.out.println("Total = " + Math.round(total));
        
         
         if (data.length>0) { average = total/data.length; }
         
-        System.out.println("Total = " + total);
-        System.out.println("Average: " + average);
+        System.out.println("Total = " + Math.round(total));
+        /* 
+        * print out the average in the main class
+        * System.out.println("Average: " + average);
+        */
         return average;
     }
+    
     static int getLargest(int[] data)
     {   
-        // code
-        return 0;
+        int largest = data[0];
+        for (int element : data)
+        {
+            if (element > largest )
+            largest = element;
+        }
+        return largest;
     }
     
     static int getSmallest(int[] data)
     {
-        // code
-        return 0;
+        int smallest = data[0];
+        for (int element : data)
+        {
+            if (element < smallest)
+            smallest = element;
+        }
+        return smallest;
     }
 }
