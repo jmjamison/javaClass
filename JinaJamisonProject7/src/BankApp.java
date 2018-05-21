f545cbc193fcf8c2fa86d780d15f3153d88ffa4f
Jamie Jamison <jamison@library.ucla.edu>
2018-05-20 22:18:31 -0700

    20180520jmj: fix the BankApp.java, line from merge

--- JinaJamisonProject7/src/BankApp.java
+++ JinaJamisonProject7/src/BankApp.java
@@ -17,10 +17,6 @@ public class BankApp {
         
         acctDatabase.createCheckingAccount("Alin Parker", "123-45-6789", 20000.0f);
         acctDatabase.createSavingsAccount("Mary Jones", "987-65-4321", 15000.0f);
-<<<<<<< HEAD
-        // invalid social security number to test
-=======
->>>>>>> 00b53839ae0e3e3aa57839872cb208778804b6bb
         acctDatabase.createSavingsAccount("John Smith", "1239-45-6789", 12000.0f);
         
         acctDatabase.print();
